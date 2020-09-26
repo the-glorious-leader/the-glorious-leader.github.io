6c53e630cc64c0a44ac1a8a46c710b448e048beb
David Geng <59180902+the-glorious-leader@users.noreply.github.com>
2020-09-25 21:07:13 -0400

    WE ARE BACK

--- login.js
+++
@@ -1,8 +0,0 @@
-function check(form){
-    if(form.userid.value == "同志" ||form.userid.value == "comrade"&& form.pswrd.value == "forthepeople"){
-        window.open("HomePage.html",target = "_self");
-    }else{
-        alert("YOU GO TO GULAG NOW");
-        window.open("stalinGulag.html","_self");  
-    }
-}
